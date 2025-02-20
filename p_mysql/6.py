80ab69ff2a678587d4619d471af8edf3afc1c987
v41nw7jl <167311253+v41nw7jl@users.noreply.github.com>
2025-02-20 10:38:34 +0000

    update

--- p_mysql/6.py
+++ p_mysql/6.py
@@ -1,4 +1,4 @@
-# simple mysql connection python code
+# fetchone() with while loop 
 
 import mysql.connector
 connection = mysql.connector.connect(host = "localhost" , user = "root" , passwd = "password" , database = "my_db")
@@ -7,8 +7,11 @@ if connection.is_connected() == False:  # if no connection made
     print('Erorr : Connection not found')
 cur = connection.cursor()   # object for cursor function
 cur.execute('select * from stu_id')     # executing sql query
+
 data = cur.fetchone()       # receving response from mysql server into data variable
-for i in data:
-    print (i)
+
+while data is not None:
+        print(data)
+        data = cur.fetchone()
 
 connection.close()  # close connection & prevent potential memory leaks
